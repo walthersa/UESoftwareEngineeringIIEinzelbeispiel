09ce9635b7e207eab3de7c14679909848c34c11c
Walther Salcher <walthersalcher@aon.at>
2018-03-10 23:42:40 +0100

    Layout fertig

--- app/src/main/java/com/example/walther/uesoftwareengineeringiieinzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/walther/uesoftwareengineeringiieinzelbeispiel/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.walther.uesoftwareengineeringiieinzelbeispiel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.button);
+        final TextView TextView = (TextView) findViewById(R.id.textView2);
+        final TextView TextViewError = (TextView) findViewById(R.id.textViewErr);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String inString = TextView.getText().toString();
+
+                if(inString.length()>5){
+                    TextViewError.setText(R.string.errNoLong);
+                }else if(isPalindrome(inString)){
+                    TextViewError.setText(R.string.isPalindrom);
+                }else{
+                    TextViewError.setText(R.string.isNoPalindrom);
+                }
+
+
+
+
+            }
+        });
+
     }
+
+    public static boolean isPalindrome(String str)	{
+        return str.equals(new StringBuilder(str).reverse().toString());
+    }
+
+
 
 
 
