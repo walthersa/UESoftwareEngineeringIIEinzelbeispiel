4b287c580bd9b64b5239f9f419e2cbb65968c1da
Walther Salcher <walthersalcher@aon.at>
2018-03-11 01:18:15 +0100

    Aufgabe Fertig

--- app/src/main/java/com/example/walther/uesoftwareengineeringiieinzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/walther/uesoftwareengineeringiieinzelbeispiel/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,29 +15,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final Button button = findViewById(R.id.button);
-        final TextView TextView = (TextView) findViewById(R.id.textView2);
-        final TextView TextViewError = (TextView) findViewById(R.id.textViewErr);
+        final EditText EditText = findViewById(R.id.editText);
+        final TextView TextViewError = findViewById(R.id.textViewErr);
 
 
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String inString = TextView.getText().toString();
 
-                if(inString.length()>5){
+            public void onClick(View v) {
+                String inString = EditText.getText().toString();
+                if(inString.length()<5){
                     TextViewError.setText(R.string.errNoLong);
-                }else if(isPalindrome(inString)){
-                    TextViewError.setText(R.string.isPalindrom);
-                }else{
-                    TextViewError.setText(R.string.isNoPalindrom);
+
+                }else {
+                    if (isPalindrome(inString)) {
+                        TextViewError.setText(R.string.isPalindrom);
+                    } else {
+                        TextViewError.setText(R.string.isNoPalindrom);
+                    }
                 }
 
 
 
+               // TextViewError.setText(inString);
 
             }
         });
 
+
+
     }
+
+
+
+
 
     public static boolean isPalindrome(String str)	{
         return str.equals(new StringBuilder(str).reverse().toString());
